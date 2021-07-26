81de7ed870869cc17d8f38fd046096b01948dd20
cemachelen <h.l.burns@leeds.ac.uk>
2021-07-26 11:22:00 +0100

    :hammer_and_wrench: scripts used in dry run 1. started separating out globabl and CP for issue #51

--- Ensembles/README.md
+++ Ensembles/README.md
@@ -1,1 +1,7 @@
+
+
 https://journals.ametsoc.org/view/journals/mwre/136/1/2007mwr2123.1.xml
+
+run automation 
+
+` ./dry_run.sh > logs 2> err &`
--- Ensembles/dry_run.sh
+++ Ensembles/dry_run.sh
@@ -7,8 +7,8 @@ echo -e "test long run sleep script\n" > output.txt
 START="$(date +%s)"
 url=/gws/nopw/j04/swift/public/requests/SWIFT_TB3/july_dryrun/plotting_suite/
 old_nofolders=$(ls -d ${url}*/ | wc -l)
-old_nofolders=0
-for i in $(seq  1 600)
+old_nofolders=2
+for i in $(seq  1 1000)
  do   
     # sleep for 5 mins
     sleep $((60*5)) 
@@ -22,21 +22,33 @@ for i in $(seq  1 600)
 	then
     echo new folder found >> output.txt
     echo $(ls -d ${url}*/) >> output.txt
-    new_dir=$(ls -td -- ${url}/*/ | head -n 1 | cut -d'/' -f11)
+    new_dir=$(ls -td -- ${url}*/ | head -n 1 | cut -d'/' -f11)
     #new_dir=$(ls -td -- ${url}*/ | head -n 1 | cut -d'/' -f2) 
     date=$(echo $new_dir | cut -d'T' -f1)
     hr=$(echo $new_dir | cut -d'T' -f2)
-    hr=$(echo $hr | cut -d'0' -f1)
-    echo hr is ${hr} >> output.txt
-    echo new dir is ${new_dir} >>output.txt
-    for i in $(seq  1 600)
+    if [[ "${hr}" = "0000Z" ]]; then
+     hr=00
+    else
+	hr=$(echo $hr | cut -d'0' -f1)
+    fi
+    echo hr is "${hr}" >> output.txt
+    echo new dir is "${new_dir}" >>output.txt
+    pptsdone="N"
+    for i in $(seq  1 200)
     do
 	echo waiting for files
+	no_files=$(ls ${url}/${new_dir}/* | wc -l)
+	if (( ${no_files} < 41300  )); then
 	sleep $((60*5))
-	no_files=$(ls $url/$new_dir | wc -l)
-	if [[ "${no_files}" = "41363"  ]]; then
-	    echo all files found, start ppt gen
-	    ./test.sh -d "${date}" -t "${hr}"
+	fi
+	if (( ${no_files} > 41300  )); then
+	    if [[ "${pptsdone}" = "N" ]]; then
+		echo nearly all files found wait 5 mins then run
+		sleep $((60*5))
+		echo all files found, start ppt gen
+		./test.sh -d "${date}" -t "${hr}"
+		pptsdone="Y"
+	    fi
 	fi
     done
     fi
